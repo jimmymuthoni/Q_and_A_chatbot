c34b6fe4906e743a917f724177ecf398ff5c9ad2
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-17 18:32:05 +0300

    streamlit ui for interacting with models

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import os
 load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY ")
 
 #setting langsmith tracking
 os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
@@ -23,10 +24,31 @@ prompt = ChatPromptTemplate.from_messages(
 
 
 #function to generate response
-def generate_response(question, api_key, llm, temperature, max_tokens):
-    openai.api_key = api_key
-    llm = ChatOpenAI(model=llm)
+def generate_response(question,llm, temperature, max_tokens):
+    llm = ChatOpenAI(model=llm, api_key = api_key)
     ouput_parser = StrOutputParser()
     chain = prompt | llm | ouput_parser
     answer = chain.invoke({"question": question})
     return answer
+
+# setting up the UI
+st.title("Q&A Chatbot With OpenAI")
+st.sidebar.title("Settings")
+
+#drop down to select models
+llm = st.sidebar.selectbox("Select an Open AI model", ["gpt-4-o", "gpt-4-turbo", "gpt-4"])
+
+#response parametes
+temperature = st.sidebar.slider("Tempearture", min_value = 0.0, max_value = 1.0, value = 0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value = 20, max_value = 100, value = 50)
+
+
+##main interface for user input
+st.write("Go ahead and ask any question:")
+user_input = st.text_input("You:")
+if user_input:
+    response = generate_response(user_input, llm, temperature, max_tokens)
+    st.write(response)
+
+else:
+    st.write("Please provide the question.")
